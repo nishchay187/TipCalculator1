a9ab8008e6ee8d8e65e3835310d049c2f440e8db
nishchay187 <nishchaykukreja2580@gmail.com>
2021-04-01 00:33:45 +0530

    done

---
+++ TipCalculator/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace TipCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bill = new System.Windows.Forms.Label();
+            this.TipPer = new System.Windows.Forms.Label();
+            this.NumberOfPeople = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Bill
+            // 
+            this.Bill.AutoSize = true;
+            this.Bill.Location = new System.Drawing.Point(113, 61);
+            this.Bill.Name = "Bill";
+            this.Bill.Size = new System.Drawing.Size(23, 15);
+            this.Bill.TabIndex = 0;
+            this.Bill.Text = "Bill";
+            // 
+            // TipPer
+            // 
+            this.TipPer.AutoSize = true;
+            this.TipPer.Location = new System.Drawing.Point(113, 170);
+            this.TipPer.Name = "TipPer";
+            this.TipPer.Size = new System.Drawing.Size(36, 15);
+            this.TipPer.TabIndex = 1;
+            this.TipPer.Text = "Tip %";
+            // 
+            // NumberOfPeople
+            // 
+            this.NumberOfPeople.AutoSize = true;
+            this.NumberOfPeople.Location = new System.Drawing.Point(113, 286);
+            this.NumberOfPeople.Name = "NumberOfPeople";
+            this.NumberOfPeople.Size = new System.Drawing.Size(106, 15);
+            this.NumberOfPeople.TabIndex = 2;
+            this.NumberOfPeople.Text = "Number Of People";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "$0.00";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 188);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox2.MaxLength = 99;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 304);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox3.MaxLength = 100;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 188);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(31, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 187);
+            this.button2.Margin = new System.Windows.Forms.Padding(0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(83, 304);
+            this.button3.Margin = new System.Windows.Forms.Padding(0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(213, 304);
+            this.button4.Margin = new System.Windows.Forms.Padding(0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "+";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(383, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tip";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(383, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Total ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(383, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 15);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "per person";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(383, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "per person";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(504, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(504, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.NumberOfPeople);
+            this.Controls.Add(this.TipPer);
+            this.Controls.Add(this.Bill);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Bill;
+        private System.Windows.Forms.Label TipPer;
+        private System.Windows.Forms.Label NumberOfPeople;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ TipCalculator/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //This will initialize input textboxes and output labels.
+            textBox1.Text = "$0.00";
+            textBox2.Text = "0%";
+            textBox3.Text = "0";
+            label5.Text = "$0.00";
+            label6.Text = "$0.00";
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //This will check the input in textbox1 (Bill) that it has only digits
+            if (!char.IsNumber(e.KeyChar) )
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //This will check the input in textbox3 (Number Of Persons) that it has only digits
+            if (!char.IsNumber(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //This will check the input in textbox1 (Bill) that it has only digits and decimal only or not.
+            if (!char.IsNumber(e.KeyChar) & e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox2.Text;
+            //This will remove the percentage(%) symbol from the string so that it can be converted in integer.
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == '%')
+                {
+                    a = a.Replace("%", "");
+                }
+            }
+            int b = Int16.Parse(a);
+            //This will decrease the tip % by 1 if the value of tip % is greater than 0.
+            if (b > 0)
+            {
+                b = b - 1;
+            }
+            textBox2.Text = b.ToString() + "%";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string a = textBox2.Text;
+            //This will remove the percentage(%) symbol from the string so that it can be converted in integer.
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == '%')
+                {
+                    a = a.Replace("%", "");
+                }
+            }
+            int b = Int16.Parse(a);
+            //This will increase the tip % by 1 if the value of tip % is less than 99.
+            if (b < 99)
+            {
+            b = b + 1;
+            }
+            textBox2.Text = b.ToString() + "%";
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Int16.Parse(textBox3.Text);
+            //This will decrease the number of persons by 1 if the value of number of persons is greater than 0.
+            if (a > 0)
+            {
+                a = a - 1;
+            }
+            textBox3.Text = a.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = Int16.Parse(textBox3.Text);
+            //This will increase the value of number of persons by 1.
+            a += 1;
+            textBox3.Text = a.ToString();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            //This will remove the dollar($) symbol from the string so that it can be converted in double.
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == '$')
+                {
+                    a = a.Replace("$", "");
+                }
+            }
+            double bill = Convert.ToDouble(a);
+            a = textBox2.Text;
+            //This will remove the percentage(%) symbol from the string so that it can be converted in integer.
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == '%')
+                {
+                    a = a.Replace("%", "");
+                }
+            }
+            int tip = Int16.Parse(a);
+            int numberOfPersons = Int16.Parse(textBox3.Text);
+
+            //This will calcuate and display the desired output.
+
+            double totalTip = (bill * tip) / 100;
+            double TipPerPerson = totalTip / numberOfPersons;
+            double BillPerPerson = (bill / numberOfPersons) + TipPerPerson;
+            label5.Text = TipPerPerson.ToString("C");
+            label6.Text = BillPerPerson.ToString("C");
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            //This will add dollar($) symbol at the beginning of the bill amount.
+            a = "$" + a;
+            textBox1.Text = a;
+        }
+    }
+}
